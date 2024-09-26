b9c92c4db17e82b1c0e9941aa6cacdedfcef0391
Krishrat00 <142259325+Krishrat00@users.noreply.github.com>
2024-09-26 23:09:39 +0530

    Add code explanations

--- test2.py.py
+++ test2.py.py
@@ -1,4 +1,30 @@
 ```python
+# This program prints a simple greeting message to the console.
+print("Hey there!")
+```
+
+## README
+
+### Index
+
+- [Introduction](#introduction)
+- [Code Overview](#code-overview)
+
+### Introduction
+
+This repository contains a simple Python program demonstrating the use of basic print statements.
+
+### Code Overview
+
+This program contains a single line of code, which prints the message "Hey there!" to the console. 
+
+```python
+# This program prints a simple greeting message to the console.
+print("Hey there!")
+```
+
+
+```python
 print("Hey there!")
 ```
 
