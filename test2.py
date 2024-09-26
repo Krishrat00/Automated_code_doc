b8fed37cb8d51b2af2e4ccc228794e22185e24f5
Krishrat00 <142259325+Krishrat00@users.noreply.github.com>
2024-09-26 23:09:38 +0530

    Add code explanations

--- test2.py
+++ test2.py
@@ -1,1 +1,27 @@
+```python
+print("Hey there!")
+```
+
+## README
+
+### Index
+
+- [Introduction](#introduction)
+- [Usage](#usage)
+
+### Introduction
+
+This repository contains a simple Python script that prints the greeting "Hey there!".
+
+### Usage
+
+To run the script, simply execute the following command in your terminal:
+
+```bash
+python your_script_name.py
+```
+
+This will print the greeting "Hey there!" to the console. 
+
+
 print("Hey there!")
