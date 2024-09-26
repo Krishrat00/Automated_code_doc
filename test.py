7430d95356a12c0848c525b717ee0c71117928b5
Krishrat00 <142259325+Krishrat00@users.noreply.github.com>
2024-09-27 00:09:12 +0530

    Add code explanations

--- test.py
+++ test.py
@@ -1,1 +1,33 @@
+```python
+# This program prints the message "Hello World" to the console.
+print("Hello World")
+```
+
+## README
+
+### Index
+
+- [Introduction](#introduction)
+- [Code Overview](#code-overview)
+- [Usage](#usage)
+
+### Introduction
+
+This repository contains a simple Python program that demonstrates basic printing functionality. 
+
+### Code Overview
+
+The code consists of a single line:
+
+```python
+print("Hello World")
+```
+
+This line uses the `print()` function to output the string "Hello World" to the console.
+
+### Usage
+
+To run the program, simply execute the Python script. This will display the message "Hello World" in the console. 
+
+
 print("Hello World")
