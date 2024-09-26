64cbc1af424c7da8afbc604546fb9b6ef64e7622
Krishrat00 <142259325+Krishrat00@users.noreply.github.com>
2024-09-27 01:41:05 +0530

    Add code explanations

--- test2.py
+++ test2.py
@@ -1,5 +1,8 @@
+# This program prints a greeting and then checks if 1 equals 1. If true, it prints "Yes", otherwise it prints "Oh no".
+
 print("Hi I am chaitali")
 if(1==1):
   print("Yes")
 else:
   print("Oh no")
+
