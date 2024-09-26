9d6c005e70e8031242ea171e286602674da839ea
Krishrat00 <142259325+Krishrat00@users.noreply.github.com>
2024-09-27 00:44:04 +0530

    Add code explanations

--- test2.py
+++ test2.py
@@ -1,5 +1,8 @@
+# This is a simple Python program that prints a message and then demonstrates a basic if-else statement.
+
 print("Hi I am chaitali")
 if(1==1):
   print("Yes")
 else:
   print("Oh no")
+
